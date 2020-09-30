485fde2eb953370bb7c21884c34555d48b139cdc
Oulsen <47483877+Oulsen@users.noreply.github.com>
2020-09-30 23:03:47 +0200

    Added shadows to testlight (House)

--- Assets/Scripts/Light/MainLighting.cs
+++ Assets/Scripts/Light/MainLighting.cs
@@ -51,7 +51,7 @@ public class MainLighting : MonoBehaviour
         Debug.Log(timer + " " + globalLight.intensity);
 
         //Below is for testing, this needs to be a separate script on every building light using information from this script to turn the lights on when its nighttime and turn them off during the day.
-        if (globalLight.intensity < 0.5f)
+        if (globalLight.intensity < 0.7f)
         {
             testLight.gameObject.SetActive(true);
         }
