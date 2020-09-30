216a6f6a9939f04d7cfa95b29b1a895157a98fcb
Oulsen <47483877+Oulsen@users.noreply.github.com>
2020-09-30 22:21:34 +0200

    Added Main Light
    Added mainlight object with the global light and the day/night script.
    Tested 1 light on a building to turn on when its night time.
    Timer for day/night is set to 60 secs, change it in the inspector if you want. Needs at least 10-15 seconds for the animation to work.

---
+++ Assets/Scripts/Light/MainLighting.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class MainLighting : MonoBehaviour
+{
+
+    [Header("Day/Night cycle settings")]
+    public Light2D globalLight;
+    public float dayNightTimer;
+    public float speed;
+    public float dayIntensity;
+    public float nightIntensity;
+
+    public Light2D testLight;
+
+    //private vars
+    private float timer;
+    [HideInInspector] public float globalTimer;
+    private float lightIntensity;
+
+    void Start()
+    {
+        timer = dayNightTimer;
+        lightIntensity = dayIntensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DayNightCycle();
+    }
+
+    private void DayNightCycle()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (lightIntensity < 0.5)
+            {
+                lightIntensity = dayIntensity;
+            }
+            else if (lightIntensity > 0.5)
+            {
+                lightIntensity = nightIntensity;
+            }
+            timer = dayNightTimer;
+        }
+        globalLight.intensity = Mathf.Lerp(globalLight.intensity, lightIntensity, speed * Time.deltaTime);
+        Debug.Log(timer + " " + globalLight.intensity);
+
+        if (globalLight.intensity < 0.5f)
+        {
+            testLight.gameObject.SetActive(true);
+        }
+        else
+        {
+            testLight.gameObject.SetActive(false);
+        }
+    }
+}
